b42ac9110022df0a86658ea740c768b0b6e02614
Kaueny de Oliveira Alves <kaueny@MacBook-Pro-de-Kaueny.local>
2020-09-18 10:23:49 -0300

    Components

--- little-dogs/src/App.js
+++ little-dogs/src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 function App() {
-  return <div>Ínicio</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
